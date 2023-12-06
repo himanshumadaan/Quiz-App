0dd2312d55a5e0b34ea8e34a51be05e7dc3a889d
Himanshu <madaanhimanshu538@gmail.com>
2023-12-06 18:54:43 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,127 @@
+export const QuizData = [
+  {
+    question: "Which language runs in a web browser?",
+    options: ["Java", "C", "Python", "JavaScript"],
+    answer: 4,
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What year was JavaScript launched?",
+    options: ["1996", "1995", "1994", "none of the above"],
+    answer: 2,
+  },
+
+  {
+    question: "What is React.js?",
+    options: [
+      "A JavaScript framework for building user interfaces",
+      "A server-side programming language",
+      "A database management system",
+      "A front-end CSS framework",
+    ],
+    answer: 1,
+  },
+
+  {
+    question: "What does JSX stand for? ",
+    options: [
+      "JavaScript XML",
+      "Java Syntax Extension ",
+      "JSON Xchange Schema",
+      "JavaScript Xerography ",
+    ],
+    answer: 1,
+  },
+
+  {
+    question:
+      "Which statement cannot be used to declare a variable in JavaScript?",
+
+    options: ["let", " const", "int", "var"],
+    answer: 3,
+  },
+
+  {
+    question: "Which of the following is an array method?",
+    options: ["Map", " Filter", "Reduce", "All of these"],
+    answer: 4,
+  },
+
+  {
+    question: "What is the full form of DOM?",
+    options: [
+      "Data Object Model",
+      "Document or Model",
+      "Document Object Model",
+      "Document Object Meduim",
+    ],
+    answer: 3,
+  },
+
+  {
+    question:
+      "What keyword is used to declare an asynchronous function in Javascript?",
+    options: ["async", "await", "setTimeout", "None of the above"],
+    answer: 1,
+  },
+
+  {
+    question: "JavaScript File Has An Extension of:",
+
+    options: [".java", ".js", ".xml", ".javascript"],
+    answer: 2,
+  },
+
+  {
+    question: "IsNaN() Evaluates And Argument To Determine if Given Value:",
+
+    options: [
+      "Is Not a Null",
+      "Is Not a New Object",
+      "Is Not a Number",
+      "None Of The Above",
+    ],
+    answer: 3,
+  },
+
+  {
+    question: "If Button is clicked .......Event Handler is invoked.",
+    options: ["OnSubmit()", "OnLoad()", "IsPostBack()", "Onclick()"],
+    answer: 4,
+  },
+
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+
+    options: ["js", "javascript", "script", "scripting"],
+    answer: 2,
+  },
+
+  {
+    question:
+      " Which Of The Dialog Box Display a Message And a Data Entry Field?",
+
+    options: ["Prompt()", "Alert()", "Confirm()", "Msg()"],
+    answer: 1,
+  },
+];
+
